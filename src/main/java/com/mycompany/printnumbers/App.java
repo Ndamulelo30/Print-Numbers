074debf61c277bd9e7d7b545f1e1afc48a6d13e3
Ndamulelo30 <141257791+Ndamulelo30@users.noreply.github.com>
2024-02-15 10:30:30 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/printnumbers/App.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.printnumbers;
+
+/**
+ *
+ * @author Sanele
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(3);
+        System.out.println(389);
+        System.out.println(7000);
+        System.out.println(7+7);
+    }
+}
